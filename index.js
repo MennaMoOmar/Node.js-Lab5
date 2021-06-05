cb08e7c16493a093e19051d6ed257f1a7b30680a
menna <menna.mohamed.omar@gmail.com>
2021-06-05 13:51:45 +0200

    edit port

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const userRouter = require('./routers/user')
 const todoRouter = require('./routers/todo')
 
 const app = express()
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
+var port = process.env.port || process.env.PORT || 3000;
+
 
 app.use(express.json())
 app.use(userRouter)
